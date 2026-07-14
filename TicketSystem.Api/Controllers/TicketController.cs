2a2f41247e3ed906e813e82b0f9ad5ce8d9c1fef
Brian Ashman <77475043+brianashman@users.noreply.github.com>
2026-07-14 00:01:58 -0400

    Create Ticket Controller

---
+++ TicketSystem.Api/Controllers/TicketController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TicketSystem.Api.Models;
+
+namespace TicketSystem.Api.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class TicketController : ControllerBase
+	{
+
+	}
+}
