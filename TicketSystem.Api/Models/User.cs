4aeada0205ec233d14511829d102935765cd50c6
Brian Ashman <77475043+brianashman@users.noreply.github.com>
2026-07-14 18:22:36 -0400

    Changed User property from Email to PasswordHash

--- TicketSystem.Api/Models/User.cs
+++ TicketSystem.Api/Models/User.cs
@@ -6,7 +6,7 @@ namespace TicketSystem.Api.Models
 	{
 		public int Id { get; set; }
 		public string Username { get; set; }
-		public string Email { get; set; }
+		public string PasswordHash { get; set; }
 		public Role Role { get; set; }
 	}
 }
