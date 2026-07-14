bf90b4e09b5a25768994b2d4b957afbd648c2517
Brian Ashman <77475043+brianashman@users.noreply.github.com>
2026-07-13 23:48:50 -0400

    Added Ticket Model, added Ticker Status & TicketPriority Enums, created folder structure

---
+++ TicketSystem.Api/Models/Ticket.cs
@@ -0,0 +1,25 @@
+﻿namespace TicketSystem.Api.Models
+{
+	public enum TicketStatus
+	{
+		Open,
+		InProgress,
+		Resolved,
+		Closed
+	}
+	public enum TicketPriority
+	{
+		Low,
+		Medium,
+		High,
+		Urgent
+	}
+	public class Ticket
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public TicketStatus Status { get; set; }
+		public TicketPriority Priority { get; set; }
+	}
+}
