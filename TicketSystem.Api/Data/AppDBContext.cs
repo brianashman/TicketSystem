8a4e0aaca51ca84291a7e040fa7bf62f16bff881
Brian Ashman <77475043+brianashman@users.noreply.github.com>
2026-07-14 21:51:15 -0400

    Created DbContext

---
+++ TicketSystem.Api/Data/AppDBContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TicketSystem.Api.Models;
+
+namespace TicketSystem.Api.Data
+{
+	public class AppDBContext(DbContextOptions<AppDBContext> options) : DbContext(options)
+	{
+		public DbSet<Ticket> Tickets => Set<Ticket>();
+	}
+}
